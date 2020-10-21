4cfead1af90e3a9f43d4e1e285b7bf4d3e52c12e
alvishwakarma <anchal.vishwakarma@skillsoft.com>
2020-10-21 12:16:41 +0530

    push

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,38 @@ import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {User} from "./entity/User";
 
-createConnection().then(async connection => {
+// createConnection().then(async connection => {
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Ashish ";
-    user.lastName = "user";
-    user.age = 32;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+//     console.log("Inserting a new user into the database...");
+//     const user = new User();
+//     user.firstName = "Ashish ";
+//     user.lastName = "user";
+//     user.age = 32;
+//     await connection.manager.save(user);
+//     console.log("Saved a new user with id: " + user.id);
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+//     console.log("Loading users from the database...");
+//     const users = await connection.manager.find(User);
+//     console.log("Loaded users: ", users);
 
-    console.log("Running Migration......: ");
-    // run migration command
-   // await connection.runMigrations();
+//     console.log("Running Migration......: ");
+//     // run migration command
+//    // await connection.runMigrations();
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+//     console.log("Here you can setup and run express/koa/any other framework.");
 
-}).catch(error => console.log(error));
+// }).catch(error => console.log(error));
+
+const getUserList = () =>{
+    createConnection().then(async connection => {
+    
+        console.log("Loading users from the database...");
+        const users = await connection.manager.find(User);
+        console.log("Loaded users: ", users);
+    
+    }).catch(error => console.log(error));
+}
+
+getUserList()
+
+export default getUserList
