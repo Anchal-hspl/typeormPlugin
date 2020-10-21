09cb13dd6e5d3a30323ff7d0c3e37dea1a24fbaf
alvishwakarma <anchal.vishwakarma@skillsoft.com>
2020-10-21 12:20:46 +0530

    tsc

--- build/index.js
+++ build/index.js
@@ -12,20 +12,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const typeorm_1 = require("typeorm");
 const User_1 = require("./entity/User");
-typeorm_1.createConnection().then((connection) => __awaiter(void 0, void 0, void 0, function* () {
-    console.log("Inserting a new user into the database...");
-    const user = new User_1.User();
-    user.firstName = "Ashish ";
-    user.lastName = "user";
-    user.age = 32;
-    yield connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
-    console.log("Loading users from the database...");
-    const users = yield connection.manager.find(User_1.User);
-    console.log("Loaded users: ", users);
-    console.log("Running Migration......: ");
-    // run migration command
-    // await connection.runMigrations();
-    console.log("Here you can setup and run express/koa/any other framework.");
-})).catch(error => console.log(error));
+// createConnection().then(async connection => {
+//     console.log("Inserting a new user into the database...");
+//     const user = new User();
+//     user.firstName = "Ashish ";
+//     user.lastName = "user";
+//     user.age = 32;
+//     await connection.manager.save(user);
+//     console.log("Saved a new user with id: " + user.id);
+//     console.log("Loading users from the database...");
+//     const users = await connection.manager.find(User);
+//     console.log("Loaded users: ", users);
+//     console.log("Running Migration......: ");
+//     // run migration command
+//    // await connection.runMigrations();
+//     console.log("Here you can setup and run express/koa/any other framework.");
+// }).catch(error => console.log(error));
+const getUserList = () => {
+    typeorm_1.createConnection().then((connection) => __awaiter(void 0, void 0, void 0, function* () {
+        console.log("Loading users from the database...");
+        const users = yield connection.manager.find(User_1.User);
+        console.log("Loaded users: ", users);
+    })).catch(error => console.log(error));
+};
+getUserList();
+exports.default = getUserList;
 //# sourceMappingURL=index.js.map
